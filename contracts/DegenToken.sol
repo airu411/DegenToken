0df95ed65292fa8896ab66b014bddca4f3d8a772
airu411 <airu411git@gmail.com>
2024-07-24 14:49:16 +0800

    Update functionalities

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -18,12 +18,45 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
     uint256 initSupply = 100 * 10**uint256(decimals());
+    mapping(string => uint256) redeemableItems;
+
+    event ItemRedeemed(address indexed to, string itemName);
 
     constructor() ERC20("Degen", "DGN") Ownable() {
       _mint(msg.sender, initSupply);
+
+      redeemableItems["Item1"] = 10;
+      redeemableItems["Item2"] = 20;
+      redeemableItems["Item3"] = 30;
+      redeemableItems["Item4"] = 10;
+      redeemableItems["Item5"] = 20;
     }
 
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
+    }
+
+    function transfer(address _to, uint256 _amount) public override returns (bool) {
+        require(balanceOf(msg.sender) >= _amount, "You don't have enough tokens to transfer.");
+        return super.transfer(_to, _amount);
+    }
+
+    function addRedeemableItem(string memory _itemName, uint256 _itemCost) public onlyOwner {
+        redeemableItems[_itemName] = _itemCost;
+    }
+
+    function redeemItem(string memory _itemName) public {
+        require(balanceOf(msg.sender) >= redeemableItems[_itemName], "You don't have enough tokens for this item.");
+        transfer(owner(), redeemableItems[_itemName]);
+        emit ItemRedeemed(msg.sender, _itemName);
+    }
+
+    function checkBalance() public view returns (uint256) {
+        return balanceOf(msg.sender);
+    }
+
+    function burn(uint256 _amount) public override {
+        require(balanceOf(msg.sender) >= _amount, "You don't have enough tokens to burn.");
+        _burn(msg.sender, _amount);
     }
 }
